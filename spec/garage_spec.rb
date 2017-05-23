c8902e943ecd7905fabc42a3bc82d79656437ece
Demetris <thegleaming@Demetriss-MBP.home>
2017-05-23 10:18:46 +0100

    updated garage test

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -8,11 +8,14 @@ describe Garage do
   let(:van) {Van.new}
 
   it "should accept a bike and fix it" do
+    broken_bike = Bike.new
     #expect garage to be empty
     expect(garage.bike_count).to eq (0)
-    #lets dock a bike in
-    garage.accept(bike)
-    #we now expect garage to have 1 bike
+    #lets break a bike
+    broken_bike.break
+    #lets dock that bike in and fix it
+    garage.accept(broken_bike)
+    #we now expect garage to have 1 fixed bike
     expect(garage.bike_count).to eq (1)
   end
 
